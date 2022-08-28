bdd4221956c08f970d3bce528755430c85392c29
marksenee <mark233891@gmail.com>
2022-08-28 23:43:46 +0900

    [Feat] 댓글 CRUD 기능 구현

--- src/components/detail/CommentListComponent.jsx
+++ src/components/detail/CommentListComponent.jsx
@@ -1,39 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import CreateIcon from "@mui/icons-material/Create";
-import {
-  StyleInputComment,
-  StyleAddComment,
-  StyleCommentItem,
-  CommentContainerStyle,
-  ContainerStyle,
-  TextStyle,
-} from "./styles";
+import { StyleCommentItem, CommentContainerStyle, TextStyle } from "./styles";
 
-function CommentListComponent() {
-  //   console.log("Comment Item", commentItem);
+function CommentListComponent({
+  commentList,
+  onDeleteComment,
+  onChangeEditStatus,
+  onUpdateComment,
+}) {
   return (
-    <StyleCommentItem>
-      <CommentContainerStyle direction="row" transePose="space-between">
-        <CommentContainerStyle direction="column" margin="5px 0px 0px 10px">
-          <TextStyle margin="7px 0px 5px 5px" fontSize="16px">
-            댓글1
-          </TextStyle>
-          <TextStyle margin="0px 0px 0px 5px" fontSize="14px">
-            작성자
-          </TextStyle>
-        </CommentContainerStyle>
-        <CommentContainerStyle direction="row" margin="10px 10px 0px 5px">
-          <IconButton aria-label="delete" size="small">
-            <CreateIcon />
-          </IconButton>{" "}
-          <IconButton aria-label="delete" size="small" color="error">
-            <DeleteIcon />
-          </IconButton>
-        </CommentContainerStyle>
-      </CommentContainerStyle>
-    </StyleCommentItem>
+    <>
+      {commentList.map((item) => (
+        <CommentItemComponent
+          key={item.id}
+          id={item.id}
+          comment={item.comment}
+          editCheck={item.editCheck}
+          userName={item.userName}
+          onDeleteComment={onDeleteComment}
+          onChangeEditStatus={onChangeEditStatus}
+          onUpdateComment={onUpdateComment}
+        />
+      ))}
+    </>
+  );
+}
+
+function CommentItemComponent({
+  id,
+  comment,
+  editCheck,
+  userName,
+  onDeleteComment,
+  onChangeEditStatus,
+  onUpdateComment,
+}) {
+  const [inputs, setInputs] = useState({
+    todoId: 1, // Id를 1로 설정했다가 2로 변경하고 setInputs도 2로 값을 변경하면 데이터 삭제시 todoid가 2인것이 전부 삭제됨...
+    userId: "",
+    comment: "",
+    userName: "",
+    createdAt: 2022,
+    editCheck: false,
+  });
+  /* 댓글 입력하기 */
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  return (
+    <>
+      {!editCheck ? (
+        <StyleCommentItem>
+          <CommentContainerStyle direction="row" transePose="space-between">
+            <CommentContainerStyle direction="column" margin="5px 0px 0px 10px">
+              <TextStyle margin="7px 0px 5px 5px" fontSize="16px">
+                {comment}
+              </TextStyle>
+              <TextStyle margin="0px 0px 0px 5px" fontSize="14px">
+                {userName}
+              </TextStyle>
+            </CommentContainerStyle>
+            <CommentContainerStyle direction="row" margin="10px 10px 0px 5px">
+              <IconButton
+                aria-label="delete"
+                size="small"
+                onClick={() => onChangeEditStatus(id)}
+              >
+                <CreateIcon />
+              </IconButton>{" "}
+              <IconButton
+                aria-label="delete"
+                size="small"
+                color="error"
+                onClick={() => onDeleteComment(id)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </CommentContainerStyle>
+          </CommentContainerStyle>
+        </StyleCommentItem>
+      ) : (
+        <StyleCommentItem>
+          <CommentContainerStyle direction="row" transePose="space-between">
+            <CommentContainerStyle direction="column" margin="5px 0px 0px 10px">
+              {/* <TextStyle margin="7px 0px 5px 5px" fontSize="16px">
+                {comment}
+              </TextStyle> */}
+              <input
+                name="comment"
+                defaultValue={comment}
+                onChange={onChange}
+              />
+              <TextStyle margin="0px 0px 0px 5px" fontSize="14px">
+                {userName}
+              </TextStyle>
+            </CommentContainerStyle>
+            <CommentContainerStyle direction="row" margin="10px 10px 0px 5px">
+              <button onClick={() => onChangeEditStatus(id)}>취소</button>
+              <button onClick={() => onUpdateComment(id, inputs)}>완료</button>
+            </CommentContainerStyle>
+          </CommentContainerStyle>
+        </StyleCommentItem>
+      )}
+    </>
   );
 }
 
