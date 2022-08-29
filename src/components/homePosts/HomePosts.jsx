af28374daad8ccfd666780bb4df701b4ec889aa1
marksenee <mark233891@gmail.com>
2022-08-29 18:35:36 +0900

    [Update] homepost 수정 사항 반영

--- src/components/homePosts/HomePosts.jsx
+++ src/components/homePosts/HomePosts.jsx
@@ -5,22 +5,20 @@ import "./HomePosts.css";
 import FixButton from "./FixBtn";
 
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function HomePosts(props) {
   const [login, setLogin] = useState(false);
   const navigate = useNavigate();
 
-  console.log(props);
-
   if (login === true) {
     return (
       <div className="postList">
-        <div
-          onClick={() => navigate(`/detail/${props.id}`)}
-          className="commentBox"
-        >
-          <h3 className="title">{props.title}</h3>
-          <p className="comment">{props.comment}</p>
+        <div className="commentBox">
+          <Link to={`/detail/${props.id}`}>
+            <h3 className="title">{props.title}</h3>
+            <p className="comment">{props.comment}</p>
+          </Link>
         </div>
       </div>
     );
@@ -31,12 +29,14 @@ function HomePosts(props) {
           onClick={() => navigate(`/detail/${props.id}`)}
           className="commentBox"
         >
-          <h3 className="title">{props.title}</h3>
-          <p className="comment">{props.comment}</p>
+          <Link to={`/detail/${props.id}`}>
+            <h3 className="title">{props.title}</h3>
+            <p className="comment">{props.comment}</p>
+          </Link>
         </div>
         <span>
-          <FixButton type={"delete"} />
-          <FixButton type={"fix"} />
+          <FixButton id={props.id} type={"delete"} />
+          <FixButton id={props.id} type={"fix"} />
         </span>
       </div>
     );
